ac0022318cd6e241e970c93037f37ce250a8e633
tvv3 <tvv363@gmail.com>
2025-02-06 16:37:52 +0200

    changed repo path in about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h2>About Page</h2>
     <p>
-      The repository for this app is available at: <a href="https://github.com/tvv3/task-manager-api-2025">Tasks Vue App Repo</a>
+      The repository for this app is available at: <a href="https://github.com/tvv3/vue3-tasks-app2---github">Tasks Vue App Repo</a>
     </p>
     <p>
       The repository for the laravel api task app is available at:  <a href="https://github.com/tvv3/task-manager-api-2025">Tasks Api Repo</a>
